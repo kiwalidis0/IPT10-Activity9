cf6e7615dd41fb0b78627693e1e74dbd95197a18
Andreas <luy.andreas@auf.edu.ph>
2024-10-15 00:46:13 +0800

    Add files via upload

---
+++ src/Models/BaseModel.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\BaseModel;
+
+class BaseModel
+{
+    protected $db;
+
+    public function __construct()  
+
+    {
+        // Global Database Connection
+        global $conn;
+        $this->db = $conn;        
+    }
+
+    public function fill($payload)
+    {
+        foreach ($payload as $key => $value)
+        {
+            if (property_exists($this, $key)) {
+                $this->$key = $value;
+            }
+        }
+    }
+}
